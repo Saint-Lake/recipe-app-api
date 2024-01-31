a741c80c7e4d245ccbd624a34e7259bdfd840ece
Lake Setser <lake.dalton.setser@gmail.com>
2024-01-30 21:44:19 -0600

    Fixed Linting errors push should work

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -13,10 +13,10 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
-
 
 
 class PublicUserApiTests(TestCase):
@@ -31,12 +31,12 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        res = self.client.post(CREATE_USER_URL,payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn('password',res.data)
+        self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
@@ -60,7 +60,9 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -109,6 +111,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class PrivateUserApiTests(TestCase):
     """Test API requests that require authentication."""
     def setUp(self):
@@ -123,7 +126,7 @@ class PrivateUserApiTests(TestCase):
     def test_retrieve_profile_sucess(self):
         """Test retrieval of the profile for logged in user"""
         res = self.client.get(ME_URL)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             'name': self.user.name,
@@ -138,8 +141,8 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
-        payload = {'name': 'Updated name', 'password': 'newpassword123456',}
-        
+        payload = {'name': 'Updated name', 'password': 'newpassword123456', }
+
         res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
